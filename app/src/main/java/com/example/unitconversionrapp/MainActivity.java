ed0fa186b923de6596b9e04e85e7f5e4e87714f5
Daniels1610 <danielagraz1610@hotmail.com>
2023-04-18 04:03:20 -0700

    Add activity_main.xml with drawables and Update of colors and strings files

--- app/src/main/java/com/example/unitconversionrapp/MainActivity.java
+++ app/src/main/java/com/example/unitconversionrapp/MainActivity.java
@@ -5,10 +5,20 @@ import androidx.lifecycle.ViewModelProviders;
 
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.unitconversionrapp.Entities.User;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner spinnerInitial;
+    Spinner spinnerFinal;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +27,33 @@ public class MainActivity extends AppCompatActivity {
 
         ConversionViewModel viewModel = ViewModelProviders.of(this).get(ConversionViewModel.class);
 
-        User Ximena = new User("Ximena");
+        spinnerInitial = findViewById(R.id.SpinnerInitial);
+        spinnerFinal = findViewById(R.id.SpinnerConverted);
 
-        viewModel.insertUser(Ximena);
+//        User Ximena = new User("Ximena");
+//
+//        viewModel.insertUser(Ximena);
+
+        String[] items = getResources().getStringArray(R.array.unit_array);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        spinnerInitial.setAdapter(adapter);
+        spinnerFinal.setAdapter(adapter);
+
+        spinnerInitial.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // Handle the user's selection here
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Do nothing
+            }
+        });
 
     }
+
+
+
+
 }
