ad72b9c6e91ab4e54411dcc3b896e3c8a63e5a51
Daniels1610 <danielagraz1610@hotmail.com>
2023-04-18 05:09:38 -0700

    Modify MainActivity and Add UnitConverter handler to manage conversion

--- app/src/main/java/com/example/unitconversionrapp/MainActivity.java
+++ app/src/main/java/com/example/unitconversionrapp/MainActivity.java
@@ -8,52 +8,50 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 
-import com.example.unitconversionrapp.Entities.User;
+import com.example.unitconversionrapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     Spinner spinnerInitial;
     Spinner spinnerFinal;
 
+    Button calculateBtn;
 
+    EditText firstUQ;
+
+    ActivityMainBinding binding;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         ConversionViewModel viewModel = ViewModelProviders.of(this).get(ConversionViewModel.class);
 
-        spinnerInitial = findViewById(R.id.SpinnerInitial);
-        spinnerFinal = findViewById(R.id.SpinnerConverted);
+        spinnerInitial = binding.SpinnerInitial;
+        spinnerFinal = binding.SpinnerConverted;
+        calculateBtn = binding.button;
+        firstUQ = binding.edFirstUQ;
 
-//        User Ximena = new User("Ximena");
-//
-//        viewModel.insertUser(Ximena);
 
         String[] items = getResources().getStringArray(R.array.unit_array);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
         spinnerInitial.setAdapter(adapter);
         spinnerFinal.setAdapter(adapter);
 
-        spinnerInitial.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                // Handle the user's selection here
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                // Do nothing
-            }
-        });
-
     }
 
+    public double getConversion(){
+        String spInitialValue = spinnerInitial.getSelectedItem().toString();
+        String spFinalValue = spinnerFinal.getSelectedItem().toString();
+        UnitConverter converter = new UnitConverter();
+        return converter.getConversion(Double.parseDouble(firstUQ.getText().toString()), spInitialValue, spFinalValue);
 
-
-
+    }
 }
